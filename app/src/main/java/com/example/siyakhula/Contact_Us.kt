ac2e3f2c692acf4d00b46f43c13eea6b864f310d
LittleGiant476 <102793122+LittleGiant476@users.noreply.github.com>
2024-09-08 19:21:50 +0200

    Commenting

---
+++ app/src/main/java/com/example/siyakhula/Contact_Us.kt
@@ -0,0 +1,38 @@
+package com.example.siyakhula
+
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class Contact_Us : AppCompatActivity() {
+
+    private lateinit var contactDonateBtn: Button
+    private lateinit var returnContactBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_us)
+
+        contactDonateBtn = findViewById(R.id.contactDonateBtn)
+        returnContactBtn = findViewById(R.id.returnContactBtn)
+
+        contactDonateBtn.setOnClickListener{
+            startActivity(Intent(this , Donations::class.java))
+        }
+
+        //Saving this block of code to use for the image buttons
+      /*  val url = "https://account.stewardship.org.uk/gift/start/20327069?donationType=OneOff"
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
+        */
+
+        returnContactBtn.setOnClickListener{
+            startActivity(Intent(this , Dashboard::class.java))
+        }
+    }
+}
