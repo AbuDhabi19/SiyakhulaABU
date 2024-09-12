9753f15f918fab499ccf51323eb821192b67bf0f
LittleGiant476 <102793122+LittleGiant476@users.noreply.github.com>
2024-09-13 00:13:22 +0200

    Finishing up the Image Buttons on the Contact Page

--- app/src/main/java/com/example/siyakhula/Contact_Us.kt
+++ app/src/main/java/com/example/siyakhula/Contact_Us.kt
@@ -14,6 +14,10 @@ class Contact_Us : AppCompatActivity() {
     private lateinit var contactDonateBtn: Button
     private lateinit var returnContactBtn: Button
 
+    private lateinit var instagram_button: ImageButton
+    private lateinit var facebook_button: ImageButton
+    private lateinit var youtube_button: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_us)
@@ -21,16 +25,47 @@ class Contact_Us : AppCompatActivity() {
         contactDonateBtn = findViewById(R.id.contactDonateBtn)
         returnContactBtn = findViewById(R.id.returnContactBtn)
 
+        instagram_button = findViewById(R.id.instagram_button)
+        facebook_button = findViewById(R.id.facebook_button)
+        youtube_button = findViewById(R.id.youtube_button)
+
         contactDonateBtn.setOnClickListener{
             startActivity(Intent(this , Donations::class.java))
         }
 
-//        //Saving this block of code to use for the image buttons
-        val url = "https://account.stewardship.org.uk/gift/start/20327069?donationType=OneOff"
+        //Saving this block of code to use for the image buttons
+      /*  val url = "https://account.stewardship.org.uk/gift/start/20327069?donationType=OneOff"
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(url)
         startActivity(intent)
+        */
 
+        instagram_button.setOnClickListener {
+
+            val url = "https://www.instagram.com/siyakhula.sa/?hl=en"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+
+        }
+
+        facebook_button.setOnClickListener {
+
+            val url = "https://www.facebook.com/SiyakhulaSA"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+
+        }
+
+        youtube_button.setOnClickListener {
+
+            val url = "https://www.youtube.com/@SiyakhulaSA"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+
+        }
 
         returnContactBtn.setOnClickListener{
             startActivity(Intent(this , Dashboard::class.java))
